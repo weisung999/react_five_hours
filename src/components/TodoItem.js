2c69ca2cb5a4d1bb4777f62094d1f6d573ed82f9
維菘 <weisung@weisongde-mbp.iiiedu.local>
2020-10-28 19:00:02 +0800

    todo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,7 +4,11 @@ function TodoItem(props) {
     return (
         <div className="todo-list">
             <div className="todo-item">
-                <input type="checkbox" checked={props.item.completed} />
+                <input 
+                    type="checkbox"
+                    checked={props.item.completed} 
+                    onChange={() => props.handleChange(props.item.id)}
+                />
                 <p>{props.item.text}</p>
             </div>
         </div>   
